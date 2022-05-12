8c6297f3c41d12f7e69a21683634646a02e3f62b
yaroslav.dehtiar <yaroslav.dehtiar@nixsolutions.com>
2022-05-12 06:40:08 +0300

    Add func and git actions

---
+++ AuthorizationFunc/AuthorizationFunc/Clients/Interfaces/IAuthorizationFuncClient.cs
@@ -0,0 +1,13 @@
+﻿using AuthorizationFunc.Models;
+using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthorizationFunc.Clients.Interfaces
+{
+    public interface IAuthorizationFuncClient
+    {
+        Task<HttpStatusCode> MakeAuthorizationRequest(AuthorizationData authorizationData, ILogger log);
+    }
+}
---
+++ AuthorizationFunc/AuthorizationFunc/Parsers/FormDataParser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AuthorizationFunc.Parsers.Interfaces;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AuthorizationFunc.Parsers
+{
+    public class FormDataParser : IFormDataParser
+    {
+        public string ParseValue(string key, string body)
+        {
+            string[] splitedKeys = body.Split("----------------------------");
+            var withNeededValue = splitedKeys.Where(x => x.Contains(key)).FirstOrDefault();
+            string[] withNeededValueAdditionalSplit = withNeededValue.Split("Disposition: form-data; name=");
+            var pairAfterSeparate = withNeededValueAdditionalSplit.Where(x => x.Contains(key)).FirstOrDefault();
+            return pairAfterSeparate.Split("\r\n").Where(x => !x.Contains(key) && !x.IsNullOrEmpty()).FirstOrDefault();
+        }
+    }
+}
